bfae167e2749509a8594d6fca91f76d7a195a3a0
Ali Emre AYGÜN <81041852+Alie017@users.noreply.github.com>
2023-07-17 14:01:22 +0300

    Delete src directory

--- src/modules/auth/index.js
+++
@@ -1,4 +0,0 @@
-export { default as PatientSignup } from './patient/Signup/Signup'
-export { default as PatientSignin } from './patient/Signin/Signin'
-export { default as PharmacistSignup } from './pharmacist/Signup/Signup'
-export { default as PharmacistSignin } from './pharmacist/Signin/Signin'
--- src/routes/auth.js
+++
@@ -1,8 +0,0 @@
-import { PatientSignup, PatientSignin, PharmacistSignup, PharmacistSignin } from '../modules/auth'
-
-export const authRoutes = [
-  { path: '/signup/pharmacist', Element: PharmacistSignup },
-  { path: '/signin/pharmacist', Element: PharmacistSignin },
-  { path: '/signup/patient', Element: PatientSignup },
-  { path: '/signin/patient', Element: PatientSignin },
-]
