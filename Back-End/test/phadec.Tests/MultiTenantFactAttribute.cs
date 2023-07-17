f0f4b9b9b641be16126dccd26882366ac0057229
Ali Emre AYGÜN <81041852+Alie017@users.noreply.github.com>
2023-07-17 14:13:34 +0300

    Add files via upload

---
+++ Back-End/test/phadec.Tests/MultiTenantFactAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace phadec.Tests
+{
+    public sealed class MultiTenantFactAttribute : FactAttribute
+    {
+        public MultiTenantFactAttribute()
+        {
+            if (!phadecConsts.MultiTenancyEnabled)
+            {
+                Skip = "MultiTenancy is disabled.";
+            }
+        }
+    }
+}
---
+++ Back-End/test/phadec.Web.Tests/phadecWebTestModule.cs
@@ -0,0 +1,38 @@
+﻿using Abp.AspNetCore;
+using Abp.AspNetCore.TestBase;
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+using phadec.EntityFrameworkCore;
+using phadec.Web.Startup;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+
+namespace phadec.Web.Tests
+{
+    [DependsOn(
+        typeof(phadecWebMvcModule),
+        typeof(AbpAspNetCoreTestBaseModule)
+    )]
+    public class phadecWebTestModule : AbpModule
+    {
+        public phadecWebTestModule(phadecEntityFrameworkModule abpProjectNameEntityFrameworkModule)
+        {
+            abpProjectNameEntityFrameworkModule.SkipDbContextRegistration = true;
+        } 
+        
+        public override void PreInitialize()
+        {
+            Configuration.UnitOfWork.IsTransactional = false; //EF Core InMemory DB does not support transactions.
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(typeof(phadecWebTestModule).GetAssembly());
+        }
+        
+        public override void PostInitialize()
+        {
+            IocManager.Resolve<ApplicationPartManager>()
+                .AddApplicationPartsIfNotAddedBefore(typeof(phadecWebMvcModule).Assembly);
+        }
+    }
+}
